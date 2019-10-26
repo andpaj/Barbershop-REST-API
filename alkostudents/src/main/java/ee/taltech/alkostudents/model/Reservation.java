44fd807cefa59dd8d24b724d554f17e9b76758fb
markit <markit@ttu.ee>
2019-10-27 01:08:43 +0300

    service in reservation -> list

--- alkostudents/src/main/java/ee/taltech/alkostudents/model/Reservation.java
+++ alkostudents/src/main/java/ee/taltech/alkostudents/model/Reservation.java
@@ -1,5 +1,6 @@
 package ee.taltech.alkostudents.model;
 
+import ee.taltech.alkostudents.pojo.ReservationDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,11 +23,11 @@ public class Reservation {
     private String lastName;
     private String email;
     private Long phone;
-    private String services;
+    private List<Services> services;
     private String date;
     private String time;
 
-    public Reservation(Long id, String firstName, String lastName, String email, Long phone, String services, String date, String time) {
+    public Reservation(Long id, String firstName, String lastName, String email, Long phone, List<Services> services, String date, String time) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -34,6 +36,17 @@ public class Reservation {
         this.services = services;
         this.date = date;
         this.time = time;
+    }
+
+    public Reservation(ReservationDto reservationDto) {
+        this.id = reservationDto.getId();
+        this.firstName = reservationDto.getFirstName();
+        this.lastName = reservationDto.getLastName();
+        this.email = reservationDto.getEmail();
+        this.phone = reservationDto.getPhone();
+        this.services = reservationDto.getServices();
+        this.date = reservationDto.getDate();
+        this.time = reservationDto.getTime();
     }
 
 }
