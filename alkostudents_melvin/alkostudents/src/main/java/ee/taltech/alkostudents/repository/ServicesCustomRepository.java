8eb4e89d2589bfd5fba1712bea4a6561a59eef17
melkri <melkri@ttu.ee>
2019-10-27 01:22:19 +0200

    1

--- alkostudents_melvin/alkostudents/src/main/java/ee/taltech/alkostudents/repository/ServicesCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.taltech.alkostudents.repository;
-
-import ee.taltech.alkostudents.model.Services;
-import java.util.List;
-
-public interface ServicesCustomRepository {
-
-    List<Services> findByName2(String name);
-}
